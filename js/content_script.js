f10be670e9a960fb6d0a5653e23b467e6a9db3c0
M-Taguchi <ponpocori.chiccyorina@gmail.com>
2023-07-10 22:00:49 +0900

    chromeでbingを読み込めるように修正

--- js/content_script.js
+++ js/content_script.js
@@ -1,17 +1,44 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   switch(request.type) {
-    case "CLICK_POPUP":
-      // const textarea = document.querySelector("#prompt-textarea");
-      const actionbar = document.querySelector("cib-serp").shadowRoot.querySelector("cib-action-bar").shadowRoot;
-      const textarea = actionbar.querySelector("cib-text-input").shadowRoot.querySelector("#searchbox");
-      console.log(textarea);
-      const submitBtn = actionbar.querySelector("button[aria-label='送信']");
-      textarea.value = "テスト";
-      textarea.dispatchEvent(new Event('input', {
-        bubbles: true
-      }));
-      submitBtn.click();
-      // textarea.nextElementSibling.click();
+    case "GENERATE_TITLE":
+      generateFromChat("テスト1")
       break;
+    case "GENERATE_COMPOSITION":
+      generateFromChat("テスト2")
+      break;
+    case "GENERATE_ARTICLE":
+      generateFromCompose("テスト3")
+      break;
+    default:
+      // ChatGPT用
+      // const textarea = document.querySelector("#prompt-textarea");
+      // const submitBtn = actionbar.querySelector("button[aria-label='送信']");
+      // textarea.value = "テスト";
+      // textarea.dispatchEvent(new Event('input', {
+        // bubbles: true
+      // }));
+      // submitBtn.click();
+      // textarea.nextElementSibling.click();
   }
 });
+
+function generateFromChat(str) {
+  const actionbar = document.querySelector("cib-serp").shadowRoot.querySelector("cib-action-bar").shadowRoot;
+  const textarea = actionbar.querySelector("cib-text-input").shadowRoot.querySelector("#searchbox");
+  const submitBtn = actionbar.querySelector("button[aria-label='送信']");
+  textarea.value = str;
+  textarea.dispatchEvent(new Event('input', {
+    bubbles: true
+  }));
+  submitBtn.click();
+};
+
+function generateFromCompose(str) {
+  const textarea = document.querySelector("#prompt_text");
+  const submitBtn = document.querySelector("#compose_button");
+  textarea.value = str;
+  textarea.dispatchEvent(new Event('input', {
+    bubbles: true
+  }));
+  submitBtn.click();
+}
--- js/popup.js
+++ js/popup.js
@@ -10,8 +10,31 @@ $(function(){
         });
     });
     $("#button").on("click",() => {
+        const genre = $("#genre").val();
         chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-            chrome.tabs.sendMessage(tabs[0].id, { type: "CLICK_POPUP" });
+            switch(genre) {
+                case "1":
+                    if(tabs[0].url !== "https://edgeservices.bing.com/edgesvc/chat") {
+                        alert("urlを確認してください。");
+                        break;
+                    }
+                    chrome.tabs.sendMessage(tabs[0].id, { type: "GENERATE_TITLE" });
+                    break;
+                case "2":
+                    if(tabs[0].url !== "https://edgeservices.bing.com/edgesvc/chat") {
+                        alert("urlを確認してください。");
+                        break;
+                    }
+                    chrome.tabs.sendMessage(tabs[0].id, { type: "GENERATE_COMPOSITION" });
+                    break;
+                case "3":
+                    if(tabs[0].url !== "https://edgeservices.bing.com/edgesvc/compose") {
+                        alert("urlを確認してください。");
+                        break;
+                    }
+                    chrome.tabs.sendMessage(tabs[0].id, { type: "GENERATE_ARTICLE" });
+                    break;
+            }
         });
     });
 });
